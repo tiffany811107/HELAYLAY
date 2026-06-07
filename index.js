c1595f251dd06a9811aad0a567f90493b72352d9
tiffany811107 <tiffany811107@gmail.com>
2026-06-07 14:58:04 +0800

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
+client.once('ready', () => {
+  console.log(`✅ 已登入為 ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.content === '!ping') {
+    message.reply('Pong!');
+  }
+});
+
 client.login(process.env.BOT_TOKEN);
